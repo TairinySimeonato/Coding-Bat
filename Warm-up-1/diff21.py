2ef3a5ffe5afeaf551a234f60303795dc183ac9a
TairinySimeonato <taisimeonato@gmail.com>
2016-06-26 18:28:40 -0300

    Update diff21.py

--- Warm-up-1/diff21.py
+++ Warm-up-1/diff21.py
@@ -5,6 +5,7 @@ def diff21(n):
   if n <= 21:
 # return its absolute difference between n and 21:
     return abs(21 - n)
+    
 # If the int is greater than 21:
   else:
 # return a value that is double the absolute difference between n and 21:
