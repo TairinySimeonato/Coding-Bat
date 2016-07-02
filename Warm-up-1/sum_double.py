18ca88e6c6967553ef5981b25c8c50da0dfd664c
TairinySimeonato <taisimeonato@gmail.com>
2016-07-02 17:13:57 -0300

    Create sum_double.py

---
+++ Warm-up-1/sum_double.py
@@ -0,0 +1,10 @@
+def sum_double(a, b):
+  sum = a + b
+  
+  # Given two int values, return their sum.
+  if a != b:
+    return sum
+  
+  # Unless the two values are the same, then return double their sum.
+  else:
+    return sum*2
