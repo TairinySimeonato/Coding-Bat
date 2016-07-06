4fee01dded7cb435567848ce03c5761cb374db2b
TairinySimeonato <taisimeonato@gmail.com>
2016-07-06 00:02:48 -0300

    Create same_first_last.py

---
+++ List-1/same_first_last.py
@@ -0,0 +1,6 @@
+# Given an array of ints, return True if the array is length 1 or more, and the first element and the last element are equal.
+
+def same_first_last(nums):
+  if (len(nums) >= 1) and (nums[-1] == nums[0]):
+    return True
+  return False
