faa5a0f1a680c5587fd4380def79774b3e10da46
TairinySimeonato <taisimeonato@gmail.com>
2016-07-19 12:45:11 -0300

    Create string_match.py

---
+++ Warmup-2/string_match.py
@@ -0,0 +1,18 @@
+
+# REDO THISdef string_match(a, b):
+  matches = 0
+  
+  string_to_check = a
+  if len(b) < len(a):
+    string_to_check = b
+    
+  for idx, val in enumerate(a):
+    if idx+1 >= len(a) or idx+1 >= len(b):
+      break
+    
+    if a[idx:idx+2] == b[idx:idx+2]:
+      matches += 1
+  
+  return matches
+
+# REDO THIS
